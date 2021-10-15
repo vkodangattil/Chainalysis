2cb42cc0b2c9395048635cb8ac5aa7b53c608009
Vishnu Kodangattil <vishnuko@gmail.com>
2021-10-14 21:49:10 -0400

    Initialize project using Create React App

---
+++ chainalysis/chainalysis/src/main/java/com/example/chainalysis/ChainalysisApplication.java
@@ -0,0 +1,13 @@
+package com.example.chainalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChainalysisApplication.class, args);
+	}
+
+}
---
+++ chainalysis/chainalysis/src/test/java/com/example/chainalysis/ChainalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chainalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
